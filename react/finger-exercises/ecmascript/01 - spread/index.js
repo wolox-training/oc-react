125bb3312d408abf2ad8b90f3128e5c8b52ffb3f
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-06-24 14:55:49 -0300

    test-01.1 done

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,8 @@
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(array) {
+  isArray(array);
+  return Math.min(array);
 }
 
 export function copy() {
