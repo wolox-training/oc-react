a0bf3ec4b3c7480e06833cafa7d2e55fe2596b8b
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-06-26 11:33:43 -0300

    test 01 promises done

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,13 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const promise1 = new Promise((resolve) => {
+    const start = Date.now();
+    setInterval(() => {
+      resolve(Date.now() - start);
+    }, time);
+  });
+  return promise1;
 }
 
 export function asyncDelay() {
