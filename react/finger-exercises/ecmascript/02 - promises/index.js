8b09c6160a0246df6d53b237b88be6f494a2394a
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-06-26 14:33:33 -0300

    test 2 promises

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,11 +1,16 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
+const MAXTIME = 25000;
 
 export function delay(time) {
   const promise1 = new Promise((resolve) => {
     const start = Date.now();
-    setInterval(() => {
-      resolve(Date.now() - start);
-    }, time);
+    if (time < MAXTIME) {
+      setInterval(() => {
+        resolve(Date.now() - start);
+      }, time);
+      return promise1;
+    }
+    throw new Error('This time is too much !');
   });
   return promise1;
 }
