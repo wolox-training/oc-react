1079f2ee4e46fdf1359e9c326e6477b23ebbc532
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-01 09:55:53 -0300

    set state

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-const Square = ({ value }) => (
-  <button type="button" className={styles.square}>
-    {value}
-  </button>
-);
+class Square extends React.Component {
+  state = {
+    value:null,
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <button type="button" className={styles.square} onClick={() => alert('click')}>
+        {value}
+      </button>
+    );
+  }
+}
 
 export default Square;
