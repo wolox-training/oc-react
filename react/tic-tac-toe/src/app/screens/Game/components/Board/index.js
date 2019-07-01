b754359cc8ca236d78c03e1ad95e4d436db7de82
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-01 14:19:09 -0300

    update handleClick function

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -7,12 +7,16 @@ import styles from './styles.module.scss';
 class Board extends Component {
   state = {
     squares : Array(9).fill(null),
+    xIsNext: true,
   }
 
   handleClick = (i) => {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
 
