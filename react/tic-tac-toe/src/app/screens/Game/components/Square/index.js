25cf24ece6b827f8e745cdb27c6d93456f2eb67f
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-01 14:46:04 -0300

    changed square class to function

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-class Square extends React.Component {
-  render() {
-    const {onClick, index, value} = this.props;
-    return (
-      <button type="button" className={styles.square}
-      onClick={() => onClick(index)}>
-        {value}
-      </button>
-    );
-  }
+function Square({onClick, index, value}) {
+  return (
+    <button type="button" className={styles.square}
+    onClick={() => onClick(index)}>
+      {value}
+    </button>
+  );
 }
+
 
 export default Square;
