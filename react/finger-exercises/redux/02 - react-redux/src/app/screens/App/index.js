edb138e360bf560866427c1f6ed33fc34cdcf443
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-11 17:25:07 -0300

    redux 02 react-redux

--- react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
+++ react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
@@ -3,6 +3,8 @@ import store from '@redux/store';
 import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
 
+import actionsCreators from '../../../redux/book/actions';
+
 import Book from './components/Book';
 import Search from './components/Search';
 import ShoppingCart from './components/ShoppingCart';
@@ -19,20 +21,24 @@ class App extends Component {
       const { books, bookSelected } = store.getState();
       this.setState({ books, bookSelected });
     });
-    // TODO to implement the dispatch
+    store.dispatch(actionsCreators.getBooks());
   }
 
-  // TODO to implement the dispatch
-  onSearch = value => {};
+  onSearch = value => {
+    store.dispatch(actionsCreators.searchBook(value));
+  };
 
-  // TODO to implement the dispatch
-  addToCart = item => {};
+  addToCart = item => {
+    store.dispatch(actionsCreators.addToCart(item));
+  };
 
-  // TODO to implement the dispatch
-  addItem = itemId => {};
+  addItem = itemId => {
+    store.dispatch(actionsCreators.addItem(itemId));
+  };
 
-  // TODO to implement the dispatch
-  removeItem = itemId => {};
+  removeItem = itemId => {
+    store.dispatch(actionsCreators.removeItem(itemId));
+  };
 
   CONFIGURATION_BUTTON = {
     add: {
@@ -66,9 +72,9 @@ class App extends Component {
             </div>
           )}
         </div>
-        {this.state.bookSelected.length ? (
+        {this.state.bookSelected.length && (
           <ShoppingCart data={this.state.bookSelected} addItem={this.addItem} removeItem={this.removeItem} />
-        ) : null}
+        )}
         <Footer />
       </Fragment>
     );
--- react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
@@ -8,16 +8,35 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case actions.GET_BOOKS: // TODO to implement the logic
-      return { ...state };
-    case actions.ADD_TO_CART: // TODO to implement the logic
-      return { ...state };
-    case actions.ADD_ITEM: // TODO to implement the logic
-      return { ...state };
-    case actions.REMOVE_ITEM: // TODO to implement the logic
-      return { ...state };
-    case actions.SEARCH_ITEM: // TODO to implement the logic
-      return { ...state };
+    case actions.GET_BOOKS:
+      return {
+        ...state,
+        books: action.payload,
+        originalData: action.payload
+      };
+    case actions.ADD_TO_CART:
+      return {
+        ...state,
+        bookSelected: state.bookSelected.concat(action.payload)
+      };
+    case actions.ADD_ITEM:
+      return {
+        ...state,
+        bookSelected: state.bookSelected.map(book => ({
+          ...book,
+          quantity: book.quantity + (book.id === action.payload)
+        }))
+      };
+    case actions.REMOVE_ITEM:
+      return {
+        ...state,
+        bookSelected: state.bookSelected.filter(book => book.id !== action.payload)
+      };
+    case actions.SEARCH_ITEM:
+      return {
+        ...state,
+        books: state.originalData.filter(book => book.name.toLowerCase().includes(action.payload))
+      };
     default:
       return state;
   }
