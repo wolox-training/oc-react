1d506dfd542722b731320ebdee6799472eb355d3
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-22 11:40:02 -0300

    solved indentation problems

--- react/tic-tac-toe/src/app/screens/Game/components/Matches/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Matches/index.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
+import Spinner from 'react-spinkit';
+
 import MatchesService from '../../../../../services/MatchesService';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
-var Spinner = require('react-spinkit');
-
-const getWinnerClass = isWinner => isWinner ? styles.winner : '';
+const getWinnerClass = isWinner => (isWinner ? styles.winner : '');
 
 class MatchesList extends Component {
   state = {
     val: [],
     loading: true
-  }
+  };
 
-  componentDidMount (){
+  componentDidMount() {
     const match = MatchesService.getMatches();
-    match.then((resolve) => {
-      this.setState({val: resolve.data, loading: false})
+    match.then(resolve => {
+      this.setState({ val: resolve.data, loading: false });
     });
   }
 
-  render(){
+  render() {
     return (
-      <div>
+      <Fragment>
         <h1>Historial de juegos</h1>
-        {this.state.loading ? <Spinner name="circle" color="purple"/> :
-        this.state.val.map(item =>
-          <li key= {item.id}><span className={getWinnerClass(item.winner === 'player_one')} >{item.player_one}</span> -
-          <span className={getWinnerClass(item.winner === 'player_two')}>{item.player_two}</span> </li>)
+        {this.state.loading ? (
+          <Spinner name="circle" color="purple" />
+        ) :
+          this.state.val.map(item => (
+            <li key={item.id}>
+              <span className={getWinnerClass(item.winner === 'player_one')}>{item.player_one}</span> -
+              <span className={getWinnerClass(item.winner === 'player_two')}>{item.player_two}</span>{' '}
+            </li>)
+          )
         }
-      </div>
-    )
+      </Fragment>
+    );
   }
-};
-
+}
 
 export default MatchesList;
