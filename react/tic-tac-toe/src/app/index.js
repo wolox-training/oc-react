062ec815d1024ee66f200664d708c7a7659fd067
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-07-23 16:58:15 -0300

    created LoginForm

--- react/tic-tac-toe/src/app/index.js
+++ react/tic-tac-toe/src/app/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Game from './screens/Game';
+import LoginForm from './screens/Form/LoginForm';
 
 import '../scss/application.scss';
 
 function App() {
-  return <Game />;
+  submit = values => {
+    window.alert (JSON.stringify)
+  }
+  return (
+    <Fragment>
+      <Game />
+      <LoginForm onSubmit={this.submit}/>
+    </Fragment>
+  );
 }
 
 export default App;
