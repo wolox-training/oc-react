4c17094a8e293a65a98fec5f22f9c50296d320ed
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-08-13 10:46:02 -0300

    apply recompose to matches component

--- react/tic-tac-toe/src/redux/matches/reducer.js
+++ react/tic-tac-toe/src/redux/matches/reducer.js
@@ -1,31 +1,15 @@
-import Immutable from 'seamless-immutable';
+import { createReducer, completeReducer, completeState } from 'redux-recompose';
 
 import { actions } from './actions';
 
-const initialState = {
-  matches: [],
-  loading: true
+const descriptionState = {
+  matches: []
 };
 
-function reducer(state = Immutable(initialState), action) {
-  switch (action.type) {
-    case actions.GET_MATCHES:
-      return state.merge({
-        loading: !state.loading
-      });
-    case actions.GET_MATCHES_SUCCESS:
-      return state.merge({
-        matches: action.payload.matches,
-        loading: false,
-        error: null
-      });
-    case actions.GET_MATCHES_FAILURE:
-      return state.merge({
-        error: action.payload.error
-      });
-    default:
-      return state;
-  }
-}
+const initialState = completeState(descriptionState);
 
-export default reducer;
+const reducerDescription = {
+  primaryActions: [actions.GET_MATCHES]
+};
+
+export default createReducer(initialState, completeReducer(reducerDescription));
