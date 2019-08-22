6b4f8f8733e366dc401b4aeaa81bc13173e31f9a
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2019-08-22 14:31:30 -0300

    testing 1 matchers done

--- react/finger-exercises/testing_1/01 - matchers/test.js
+++ react/finger-exercises/testing_1/01 - matchers/test.js
@@ -1,15 +1,19 @@
 import pow from '.';
 
 describe('01 - matchers', () => {
-  xit('pow returns the power based on two numeric arguments', () => {
-    pow();
+  it('pow returns the power based on two numeric arguments', () => {
+    expect(pow(2, 3)).toBe(8);
   });
-  xit('pow returns undefined if there is no arguments', () => {
+  it('pow returns undefined if there is no arguments', () => {
+    expect(pow()).toBe(undefined);
   });
-  xit('pow returns undefined if there is just one argument', () => {
+  it('pow returns undefined if there is just one argument', () => {
+    expect(pow(1)).toBe(undefined);
   });
-  xit('pow returns an array of power results if array of pairs are sent as arguments', () => {
+  it('pow returns an array of power results if array of pairs are sent as arguments', () => {
+    expect(pow([2, 3], [3, 2])).toEqual([8, 9]);
   });
-  xit('pow returns undefined in the right position of the result array if pair is not as expected', () => {
+  it('pow returns undefined in the right position of the result array if pair is not as expected', () => {
+    expect(pow([2, 3], [1])).toEqual([8, undefined]);
   });
 });
